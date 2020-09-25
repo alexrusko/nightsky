cca0ae48e9fb0bfc7dcb6fd9af28f641f2ebb1ec
Alex Rusko <alexrusko1@gmail.com>
2020-09-25 11:47:14 +0200

    implementing star size, color and constellations

--- src/script.js
+++ src/script.js
@@ -3,27 +3,109 @@ const ctx = canvas.getContext('2d');
 canvas.style.transform = 'rotate(-90deg)';
 const width = canvas.width;
 const height = canvas.height;
-ctx.fillStyle = 'white';
+const starDataParsed = JSON.parse(starData);
 
 const now = new Date();
 const lat = dms2real( 47, 29 );
 const lon = dms2real( 19, 2 );
 
-JSON.parse(starData).forEach(({ id, deDeg, deMinutes, raHours, raMinutes, magnitude, spectralType }) => {
-    const raReal  = ra2real( raHours, raMinutes );
-	const decReal = dms2real( deDeg, deMinutes );
-    const [altitude, azimuth] = coordToHorizon(now, raReal, decReal, lat, lon);
+starDataParsed.forEach((data) => {
+    const { id, deDeg, deMinutes, raHours, raMinutes, magnitude, spectralType } = data;
+    const { altitude, x, y } = getPosition(raHours, raMinutes, deDeg, deMinutes)
 
-    if (altitude > 0) {
-        const radius = 300 * Math.cos(altitude * 0.0174532925);
-        const x = radius * Math.cos(azimuth * 0.0174532925);
-        const y = radius * Math.sin(azimuth * 0.0174532925);
+    if (altitude > 0 && magnitude < 6) {
+        const size = calculateSize(magnitude);
+        const color = getColor(spectralType);
         ctx.beginPath();
-        ctx.arc((width / 2) + x, (height / 2) + y, 0.04, 0, 2 * Math.PI)
+        ctx.fillStyle = color;
+        ctx.shadowColor = color;
+        ctx.shadowBlur = 5;
+        ctx.arc((width / 2) + x, (height / 2) + y, size, 0, 2 * Math.PI)
         ctx.fill();
         ctx.closePath();   
     }
 });
+
+drawConstellation();
+function drawConstellation() {
+    constellations.forEach((constellation) => {
+        constellation.split(',').forEach((part) => {
+            const starFrom = starDataParsed.filter(star => star.id == part.split(' ')[0])[0];
+            const starTo = starDataParsed.filter(star => star.id == part.split(' ')[1])[0];
+            
+            const {x: x1, y: y1} = getPosition(starFrom.raHours, starFrom.raMinutes, starFrom.deDeg, starFrom.deMinutes);
+            const {x: x2, y: y2} = getPosition(starTo.raHours, starTo.raMinutes, starTo.deDeg, starTo.deMinutes);
+            ctx.shadowBlur = 0;
+            ctx.beginPath();
+            ctx.moveTo((width / 2) + x1, (height / 2) + y1);
+            ctx.lineTo((width / 2) + x2, (height / 2) + y2);
+            ctx.strokeStyle = "white";
+            ctx.lineWidth = 0.3;
+            ctx.stroke();
+            ctx.closePath();
+        });
+    });
+}
+
+function getPosition(raHours, raMinutes, deDeg, deMinutes) {
+    const raReal  = ra2real( raHours, raMinutes );
+    const decReal = dms2real( deDeg, deMinutes );
+    const [altitude, azimuth] = coordToHorizon(now, raReal, decReal, lat, lon);
+    const radius = 300 * Math.cos(altitude * 0.0174532925);
+    const x = radius * Math.cos(azimuth * 0.0174532925);
+    const y = radius * Math.sin(azimuth * 0.0174532925);
+    return { altitude, azimuth, x, y };
+}
+
+function calculateSize(magnitude) {
+    if (magnitude < 6 && magnitude >= 5) {
+        return 0.08;
+    } else if (magnitude < 5 && magnitude >= 4) {
+        return 0.15;
+    } else if (magnitude < 4 && magnitude >= 2.5) {
+        return 0.5;
+    } else if (magnitude < 2.5 && magnitude >= 1.5) {
+        return 1;
+    } else if (magnitude < 1.5 && magnitude >= 0.5) {
+        return 2;
+    } else if (magnitude < 0.5 && magnitude >= -0.5) {
+        return 3;
+    } else if (magnitude < -0.5 && magnitude >= -1.5) {
+        return 4;
+    } else {
+        return 5;
+    }
+}
+
+function getColor(spectralType) {
+    const spectralClass = spectralType.charAt(0);
+    let color = '#fff';
+    switch (spectralClass) {
+        case 'O':
+            color = '#9bb0ff';
+            break;
+        case 'B':
+            color = '#aabfff';
+            break;
+        case 'A':
+            color = '#cad7ff';
+            break;
+        case 'F':
+            color = '#f8f7ff';
+            break;
+        case 'G':
+            color = '#fff4ea';
+            break;
+        case 'K':
+            color = '#ffd2a1';
+            break;
+        case 'M':
+            color = '#ffcc6f';
+            break;
+    }
+
+    return color;
+};
 
 // convert right ascension (hours, minutes) to degrees as real
 function ra2real( hr, min ) {
--- src/script2.js
+++ src/script2.js
@@ -4,12 +4,13 @@ canvas.style.transform = 'rotate(-90deg)'
 const width = canvas.width;
 const height = canvas.height;
 var angle = 180;
-var r = 300;
+var r = 200;
 var x = r * Math.cos(angle * 0.0174532925);
 var y = r * Math.sin(angle * 0.0174532925);
 console.log(x, y)
-
-ctx.arc((width / 2) + x, (height / 2) + y, 5, 0, 2 * Math.PI)
+ctx.shadowColor = 'red';
+ctx.shadowBlur = 10;
+ctx.arc((width / 2) + x, (height / 2) + y, 3, 0, 2 * Math.PI)
 ctx.fill();
 // ctx.beginPath();
 // ctx.moveTo(50, 50);
