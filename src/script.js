bd1a1f72b831f612a030d78cb419a053adead47b
Alex Rusko <alexrusko1@gmail.com>
2020-09-26 01:19:32 +0200

    added all constellations, added basic control panel

--- src/script.js
+++ src/script.js
@@ -1,67 +1,87 @@
-const canvas = document.getElementById('sky-canvas');
-const ctx = canvas.getContext('2d');
-canvas.style.transform = 'rotate(-90deg)';
-const width = canvas.width;
-const height = canvas.height;
+const timeInput = document.getElementById('timePicker');
+const constellationToggle = document.getElementById('constellation-toggle');
+const skyCanvas = document.getElementById('sky-canvas');
+const constellationCanvas = document.getElementById('constellation-canvas');
+const skyCtx = skyCanvas.getContext('2d');
+const constellationCtx = constellationCanvas.getContext('2d');
+const width = skyCanvas.width;
+const height = skyCanvas.height;
 const starDataParsed = JSON.parse(starData);
+let constellationsToggled = false;
 
-const now = new Date();
+let now = new Date();
 const lat = dms2real( 47, 29 );
 const lon = dms2real( 19, 2 );
 
-starDataParsed.forEach((data) => {
-    const { id, deDeg, deMinutes, raHours, raMinutes, magnitude, spectralType } = data;
-    const { altitude, x, y } = getPosition(raHours, raMinutes, deDeg, deMinutes)
+drawStars();
 
-    if (altitude > 0 && magnitude < 6) {
-        const size = calculateSize(magnitude);
-        const color = getColor(spectralType);
-        ctx.beginPath();
-        ctx.fillStyle = color;
-        ctx.shadowColor = color;
-        ctx.shadowBlur = 5;
-        ctx.arc((width / 2) + x, (height / 2) + y, size, 0, 2 * Math.PI)
-        ctx.fill();
-        ctx.closePath();   
-    }
-});
-
-drawConstellation();
-function drawConstellation() {
-    constellations.forEach((constellation) => {
-        constellation.split(',').forEach((part) => {
-            const starFrom = starDataParsed.filter(star => star.id == part.split(' ')[0])[0];
-            const starTo = starDataParsed.filter(star => star.id == part.split(' ')[1])[0];
-            
-            const {x: x1, y: y1} = getPosition(starFrom.raHours, starFrom.raMinutes, starFrom.deDeg, starFrom.deMinutes);
-            const {x: x2, y: y2} = getPosition(starTo.raHours, starTo.raMinutes, starTo.deDeg, starTo.deMinutes);
-            ctx.shadowBlur = 0;
-            ctx.beginPath();
-            ctx.moveTo((width / 2) + x1, (height / 2) + y1);
-            ctx.lineTo((width / 2) + x2, (height / 2) + y2);
-            ctx.strokeStyle = "white";
-            ctx.lineWidth = 0.3;
-            ctx.stroke();
-            ctx.closePath();
-        });
+function drawStars() {
+    const t1 = performance.now();
+    starDataParsed.forEach((data) => {
+        const { id, deDeg, deMinutes, raHours, raMinutes, magnitude, spectralType } = data;
+        const { altitude, x, y } = getPosition(raHours, raMinutes, deDeg, deMinutes)
+    
+        if (altitude > 0 && magnitude < 6) {
+            const size = calculateSize(magnitude);
+            const color = getColor(spectralType);
+            skyCtx.beginPath();
+            skyCtx.fillStyle = color;
+            skyCtx.shadowColor = color;
+            skyCtx.shadowBlur = 5;
+            skyCtx.arc((width / 2) + x, (height / 2) + y, size, 0, 2 * Math.PI)
+            skyCtx.fill();
+            skyCtx.closePath();   
+        }
     });
-}
+    const t2 = performance.now();
+    console.log('time: ' + (t2 - t1) + 'millis');
+};
+
+function clearStars() {
+    skyCtx.clearRect(0, 0, width, height);
+};
+
+function clearConstellations() {
+    constellationCtx.clearRect(0, 0, width, height);
+};
+
+function drawConstellations() {
+    constellations.forEach((constellation) => {
+        for (let i = 0; i < constellation.length; i+= 2) {
+            const starFrom = starDataParsed.filter(star => star.id === constellation[i])[0];
+            const starTo = starDataParsed.filter(star => star.id === constellation[i+1])[0];
+            const {x: x1, y: y1, altitude: altitude1 } = getPosition(starFrom.raHours, starFrom.raMinutes, starFrom.deDeg, starFrom.deMinutes);
+            const {x: x2, y: y2, altitude: altitude2 } = getPosition(starTo.raHours, starTo.raMinutes, starTo.deDeg, starTo.deMinutes);
+            if (altitude1 > 0 && altitude2 > 0) {
+                constellationCtx.shadowBlur = 3;
+                constellationCtx.shadowColor = 'white';
+                constellationCtx.beginPath();
+                constellationCtx.moveTo((width / 2) + x1, (height / 2) + y1);
+                constellationCtx.lineTo((width / 2) + x2, (height / 2) + y2);
+                constellationCtx.strokeStyle = 'white';
+                constellationCtx.lineWidth = 0.3;
+                constellationCtx.stroke();
+                constellationCtx.closePath();
+            }
+        }
+    });
+};
 
 function getPosition(raHours, raMinutes, deDeg, deMinutes) {
     const raReal  = ra2real( raHours, raMinutes );
     const decReal = dms2real( deDeg, deMinutes );
     const [altitude, azimuth] = coordToHorizon(now, raReal, decReal, lat, lon);
-    const radius = 300 * Math.cos(altitude * 0.0174532925);
-    const x = radius * Math.cos(azimuth * 0.0174532925);
-    const y = radius * Math.sin(azimuth * 0.0174532925);
+    const radius = 300 * Math.cos(altitude * Math.PI/180);
+    const x = radius * Math.cos(azimuth * Math.PI/180);
+    const y = radius * Math.sin(azimuth * Math.PI/180);
     return { altitude, azimuth, x, y };
 }
 
 function calculateSize(magnitude) {
     if (magnitude < 6 && magnitude >= 5) {
-        return 0.08;
+        return 0.1;
     } else if (magnitude < 5 && magnitude >= 4) {
-        return 0.15;
+        return 0.25;
     } else if (magnitude < 4 && magnitude >= 2.5) {
         return 0.5;
     } else if (magnitude < 2.5 && magnitude >= 1.5) {
@@ -108,40 +128,39 @@ function getColor(spectralType) {
 };
 
 // convert right ascension (hours, minutes) to degrees as real
-function ra2real( hr, min ) {
+function ra2real(hr, min) {
     return 15*(hr + min/60);
 };
 
 // convert angle (deg, min) to degrees as real
-function dms2real( deg, min ) {
+function dms2real(deg, min) {
     return deg < 0 ? deg - min/60 : deg + min/60;
 };
 
-function coordToHorizon( utc, ra, dec, lat, lon ) {
+function coordToHorizon(utc, ra, dec, lat, lon) {
     // compute hour angle in degrees
-    let ha = meanSiderealTime( utc, lon ) - ra;
-    if (ha < 0) ha = ha + 360;
+    let ha = meanSiderealTime(utc, lon) - ra;
+    if (ha < 0) {
+        ha = ha + 360;   
+    }
 
     // convert degrees to radians
-    ha  = ha*Math.PI/180
-    dec = dec*Math.PI/180
-    lat = lat*Math.PI/180
+    const haRad  = ha*Math.PI/180
+    const decRad = dec*Math.PI/180
+    const latRad = lat*Math.PI/180
 
     // compute altitude in radians
-    const sinAlt = Math.sin(dec)*Math.sin(lat) + Math.cos(dec)*Math.cos(lat)*Math.cos(ha);
+    const sinAlt = Math.sin(decRad)*Math.sin(latRad) + Math.cos(decRad)*Math.cos(latRad)*Math.cos(haRad);
     const alt = Math.asin(sinAlt);
     
     // compute azimuth in radians
     // divide by zero error at poles or if alt = 90 deg
-    const cosAz = (Math.sin(dec) - Math.sin(alt)*Math.sin(lat))/(Math.cos(alt)*Math.cos(lat));
-    const az  = Math.acos(cosAz);
+    const cosAz = (Math.sin(decRad) - Math.sin(alt)*Math.sin(latRad))/(Math.cos(alt)*Math.cos(latRad));
+    const az  = Math.acos(cosAz) * 180/Math.PI;
 
     // convert radians to degrees
     const altitude = alt*180/Math.PI;
-    let azimuth  = az*180/Math.PI;
-
-    // choose hemisphere
-	if (Math.sin(ha) > 0) azimuth = 360 - azimuth;
+    const azimuth  = Math.sin(haRad) > 0 ? 360 - az : az;
 	
 	return [altitude, azimuth];
 };
@@ -178,10 +197,30 @@ function meanSiderealTime(now, lon) {
     let mst = 280.46061837 + 360.98564736629*jd + 0.000387933*jt*jt - jt*jt*jt/38710000 + lon;
 
     // in degrees modulo 360.0
-    if (mst > 0.0) 
+    if (mst > 0.0) {
         while (mst > 360.0) mst = mst - 360.0;
-    else
-        while (mst < 0.0)   mst = mst + 360.0;
+    } else {
+        while (mst < 0.0) mst = mst + 360.0;
+    }
         
     return mst;
+}
+
+timeInput.oninput = (e) => {
+    const { value } = e.target;
+    if (value) {
+        now = new Date(value);
+        clearStars();
+        drawStars();
+        
+        if (constellationsToggled) {
+            clearConstellations();
+            drawConstellations();
+        }
+    }
+};
+
+constellationToggle.onchange = (e) => {
+    constellationsToggled = e.target.checked;
+    constellationsToggled ? drawConstellations() : clearConstellations();
 }
